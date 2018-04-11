0023920544c228d9596bcce19193c19ec6dfa0c9
bradguru <35859950+bradguru2@users.noreply.github.com>
2018-04-11 10:26:09 -0700

    Create StringAdder.java

---
+++ src/com/binary/StringAdder.java
@@ -0,0 +1,45 @@
+package com.binary;
+
+public class StringAdder {
+
+	public static String AddBinary(String a, String b)
+    {
+        int maxLen = a.length() > b.length() ? a.length() : b.length();
+        int aDigit, bDigit, carry = 0, start = maxLen;
+        int aIndex = a.length() - 1, bIndex = b.length() - 1;
+        String answer = "";
+
+        if (maxLen > 0)
+        {
+            //Allocate for carry
+            char[] result = new char[maxLen + 1];
+
+            while (start > 0)
+            {
+                aDigit = aIndex >= 0 ? a.charAt(aIndex--) - '0' : 0;
+                bDigit = bIndex >= 0 ? b.charAt(bIndex--) - '0' : 0;
+
+                //Modeled after Full Adder circuit logic
+                int sum = aDigit ^ bDigit;
+                result[start--] = (char)('0' + (sum ^ carry));
+                carry = (aDigit & bDigit) | (carry & sum);
+            }
+
+            result[start] = (char)(carry + '0');
+
+            //Removes 0's to the left of answer
+            while (start < maxLen && result[start] == '0') start++;
+
+            answer = new String(result, start, maxLen + 1 - start);
+        }
+
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		String sb1 = "1001";
+		String sb2 = "1001";
+		System.out.printf("%s + %s = %s", sb1, sb2, AddBinary(sb1, sb2));
+	}
+
+}
